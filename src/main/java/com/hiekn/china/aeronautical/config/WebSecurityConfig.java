f24f78d6317bc6a9023be425f6523e34fa3edb19
lebovin <lebovin@qq.com>
2018-11-14 14:19:01 +0800

    api

--- src/main/java/com/hiekn/china/aeronautical/config/WebSecurityConfig.java
+++ src/main/java/com/hiekn/china/aeronautical/config/WebSecurityConfig.java
@@ -15,7 +15,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http
                 .csrf()
                 .disable()
